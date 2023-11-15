a387b3ddb1d9aa7ab0ea35ae17835f1cbb7906f8
Nathan Nicholson <nathan.bytecode@gmail.com>
2023-11-15 10:48:27 +0000

    change path projects to codestar356 in urls.py file

--- codestar356/urls.py
+++ codestar356/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls')),
-    path('', RedirectView.as_view(url='/projects/')),
+    path('codestar356/', include('codestar356.urls')),
+    path('', RedirectView.as_view(url='/codestar356/')),
 ]
